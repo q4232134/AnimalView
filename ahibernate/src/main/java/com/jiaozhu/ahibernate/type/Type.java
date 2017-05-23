b27229b7a8d4df087f8744d84a4906a938a37e75
q4232134 <195313059@qq.com>
2017-05-23 10:49:39 +0800

    增加数据库框架

---
+++ ahibernate/src/main/java/com/jiaozhu/ahibernate/type/Type.java
@@ -0,0 +1,32 @@
+package com.jiaozhu.ahibernate.type;
+
+/**
+ * Created by jiaozhu on 2017/4/1.
+ */
+
+public class Type {
+    /**
+     * 字符串类型
+     */
+    public static final String TYPE_STRING = "TEXT";
+
+    /**
+     * 整数类型
+     */
+    public static final String TYPE_INTEGER = "INTEGER";
+
+    /**
+     * 布尔值类型
+     */
+    public static final String TYPE_BOOLEAN = "INT";
+
+    /**
+     * 浮点型
+     */
+    public static final String TYPE_FLOAT = "FLOAT";
+
+    /**
+     * 二进制类型
+     */
+    public static final String TYPE_BLOB = "BLOB";
+}
